77ed708bf1e9de28cd9c32b29633a473acd0ab3e
danielibac <dan_09309@hotmail.com>
2015-12-14 17:21:46 -0300

    tabla usuario

--- database/migrations/2015_12_14_200218_usuarios.php
+++ database/migrations/2015_12_14_200218_usuarios.php
@@ -12,7 +12,18 @@ class Usuarios extends Migration {
 	 */
 	public function up()
 	{
-		//
+		Schema::create('usuarios', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('nombre');
+			$table->string('apellido_p',64);
+			$table->string('apellido_m',64);
+			$table->string('email')->unique();
+			$table->string('password', 60);
+			$table->rememberToken();
+			$table->string('url_avatar', 47);
+			$table->softDeletes();
+			$table->timestamps();
+		});
 	}
 
 	/**
@@ -22,7 +33,7 @@ class Usuarios extends Migration {
 	 */
 	public function down()
 	{
-		//
+		Schema::drop('usuarios');
 	}
 
 }
